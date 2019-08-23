35f7539ef2ebb1ae94f765c9fcc1bca4dee332d0
yhsmiley <46747906+yhsmiley@users.noreply.github.com>
2019-08-23 15:47:18 +0800

    modified codes to make it run

--- lib/nets/network.py
+++ lib/nets/network.py
@@ -30,7 +30,10 @@ from model.config import cfg
 
 import tensorboardX as tb
 
-from scipy.misc import imresize
+# from scipy.misc import imresize
+from skimage.transform import resize as imresize
+import numpy
+from PIL import Image
 
 
 class Network(nn.Module):
@@ -52,7 +55,8 @@ class Network(nn.Module):
     def _add_gt_image(self):
         # add back mean
         image = self._image_gt_summaries['image'] + cfg.PIXEL_MEANS
-        image = imresize(image[0], self._im_info[:2] / self._im_info[2])
+        # image = imresize(image[0], self._im_info[:2] / self._im_info[2])
+        image = numpy.array(Image.fromarray(image[0].astype('uint8')).resize(self._im_info[:2] / self._im_info[2]))
         # BGR to RGB (opencv uses BGR)
         self._gt_image = image[np.newaxis, :, :, ::-1].copy(order='C')
 
@@ -60,73 +64,49 @@ class Network(nn.Module):
         # use a customized visualization function to visualize the boxes
         self._add_gt_image()
         image = draw_bounding_boxes(\
-                          self._gt_image, self._image_gt_summaries['gt_boxes'], self._image_gt_summaries['im_info'])
+        self._gt_image, self._image_gt_summaries['gt_boxes'], self._image_gt_summaries['im_info'])
 
-        return tb.summary.image('GROUND_TRUTH',
-                                image[0].astype('float32') / 255.0, dataformats='HWC')
+        return tb.summary.image('GROUND_TRUTH', image[0].astype('float32') / 255.0, dataformats='HWC')
 
     def _add_act_summary(self, key, tensor):
-        return tb.summary.histogram(
-            'ACT/' + key + '/activations',
-            tensor.data.cpu().numpy(),
-            bins='auto'),
-        tb.summary.scalar('ACT/' + key + '/zero_fraction',
-                          (tensor.data == 0).float().sum() / tensor.numel())
+        return tb.summary.histogram('ACT/' + key + '/activations', tensor.data.cpu().numpy(), bins='auto'), tb.summary.scalar('ACT/' + key + '/zero_fraction', (tensor.data == 0).float().sum() / tensor.numel())
 
     def _add_score_summary(self, key, tensor):
         return tb.summary.histogram(
             'SCORE/' + key + '/scores', tensor.data.cpu().numpy(), bins='auto')
 
     def _add_train_summary(self, key, var):
-        return tb.summary.histogram(
-            'TRAIN/' + key, var.data.cpu().numpy(), bins='auto')
+        return tb.summary.histogram('TRAIN/' + key, var.data.cpu().numpy(), bins='auto')
 
     def _proposal_top_layer(self, rpn_cls_prob, rpn_bbox_pred):
-        rois, rpn_scores = proposal_top_layer(\
-                                        rpn_cls_prob, rpn_bbox_pred, self._im_info,
-                                         self._feat_stride, self._anchors, self._num_anchors)
+        rois, rpn_scores = proposal_top_layer(rpn_cls_prob, rpn_bbox_pred, self._im_info, self._feat_stride, self._anchors, self._num_anchors)
         return rois, rpn_scores
 
     def _proposal_layer(self, rpn_cls_prob, rpn_bbox_pred):
-        rois, rpn_scores = proposal_layer(\
-                                        rpn_cls_prob, rpn_bbox_pred, self._im_info, self._mode,
-                                         self._feat_stride, self._anchors, self._num_anchors)
+        rois, rpn_scores = proposal_layer(rpn_cls_prob, rpn_bbox_pred, self._im_info, self._mode, self._feat_stride, self._anchors, self._num_anchors)
 
         return rois, rpn_scores
 
     def _roi_pool_layer(self, bottom, rois):
-        return RoIPool((cfg.POOLING_SIZE, cfg.POOLING_SIZE),
-                       1.0 / 16.0)(bottom, rois)
+        return RoIPool((cfg.POOLING_SIZE, cfg.POOLING_SIZE), 1.0 / 16.0)(bottom, rois)
 
     def _roi_align_layer(self, bottom, rois):
-        return RoIAlign((cfg.POOLING_SIZE, cfg.POOLING_SIZE), 1.0 / 16.0,
-                        0)(bottom, rois)
+        return RoIAlign((cfg.POOLING_SIZE, cfg.POOLING_SIZE), 1.0 / 16.0, 0)(bottom, rois)
 
     def _anchor_target_layer(self, rpn_cls_score):
         rpn_labels, rpn_bbox_targets, rpn_bbox_inside_weights, rpn_bbox_outside_weights = \
-          anchor_target_layer(
-          rpn_cls_score.data, self._gt_boxes.data.cpu().numpy(), self._im_info, self._feat_stride, self._anchors.data.cpu().numpy(), self._num_anchors)
+          anchor_target_layer(rpn_cls_score.data, self._gt_boxes.data.cpu().numpy(), self._im_info, self._feat_stride, self._anchors.data.cpu().numpy(), self._num_anchors)
 
-        rpn_labels = torch.from_numpy(rpn_labels).float().to(
-            self._device)  #.set_shape([1, 1, None, None])
-        rpn_bbox_targets = torch.from_numpy(rpn_bbox_targets).float().to(
-            self._device)  #.set_shape([1, None, None, self._num_anchors * 4])
-        rpn_bbox_inside_weights = torch.from_numpy(
-            rpn_bbox_inside_weights).float().to(
-                self.
-                _device)  #.set_shape([1, None, None, self._num_anchors * 4])
-        rpn_bbox_outside_weights = torch.from_numpy(
-            rpn_bbox_outside_weights).float().to(
-                self.
-                _device)  #.set_shape([1, None, None, self._num_anchors * 4])
+        rpn_labels = torch.from_numpy(rpn_labels).float().to(self._device)  #.set_shape([1, 1, None, None])
+        rpn_bbox_targets = torch.from_numpy(rpn_bbox_targets).float().to(self._device)  #.set_shape([1, None, None, self._num_anchors * 4])
+        rpn_bbox_inside_weights = torch.from_numpy(rpn_bbox_inside_weights).float().to(self._device)  #.set_shape([1, None, None, self._num_anchors * 4])
+        rpn_bbox_outside_weights = torch.from_numpy(rpn_bbox_outside_weights).float().to(self._device)  #.set_shape([1, None, None, self._num_anchors * 4])
 
         rpn_labels = rpn_labels.long()
         self._anchor_targets['rpn_labels'] = rpn_labels
         self._anchor_targets['rpn_bbox_targets'] = rpn_bbox_targets
-        self._anchor_targets[
-            'rpn_bbox_inside_weights'] = rpn_bbox_inside_weights
-        self._anchor_targets[
-            'rpn_bbox_outside_weights'] = rpn_bbox_outside_weights
+        self._anchor_targets['rpn_bbox_inside_weights'] = rpn_bbox_inside_weights
+        self._anchor_targets['rpn_bbox_outside_weights'] = rpn_bbox_outside_weights
 
         for k in self._anchor_targets.keys():
             self._score_summaries[k] = self._anchor_targets[k]
@@ -135,8 +115,7 @@ class Network(nn.Module):
 
     def _proposal_target_layer(self, rois, roi_scores):
         rois, roi_scores, labels, bbox_targets, bbox_inside_weights, bbox_outside_weights = \
-          proposal_target_layer(
-          rois, roi_scores, self._gt_boxes, self._num_classes)
+          proposal_target_layer(rois, roi_scores, self._gt_boxes, self._num_classes)
 
         self._proposal_targets['rois'] = rois
         self._proposal_targets['labels'] = labels.long()
@@ -153,9 +132,7 @@ class Network(nn.Module):
         # just to get the shape right
         #height = int(math.ceil(self._im_info.data[0, 0] / self._feat_stride[0]))
         #width = int(math.ceil(self._im_info.data[0, 1] / self._feat_stride[0]))
-        anchors, anchor_length = generate_anchors_pre(\
-                                              height, width,
-                                               self._feat_stride, self._anchor_scales, self._anchor_ratios)
+        anchors, anchor_length = generate_anchors_pre(height, width, self._feat_stride, self._anchor_scales, self._anchor_ratios)
         self._anchors = torch.from_numpy(anchors).to(self._device)
         self._anchor_length = anchor_length
 
@@ -171,8 +148,7 @@ class Network(nn.Module):
         in_box_diff = bbox_inside_weights * box_diff
         abs_in_box_diff = torch.abs(in_box_diff)
         smoothL1_sign = (abs_in_box_diff < 1. / sigma_2).detach().float()
-        in_loss_box = torch.pow(in_box_diff, 2) * (sigma_2 / 2.) * smoothL1_sign \
-                      + (abs_in_box_diff - (0.5 / sigma_2)) * (1. - smoothL1_sign)
+        in_loss_box = torch.pow(in_box_diff, 2) * (sigma_2 / 2.) * smoothL1_sign + (abs_in_box_diff - (0.5 / sigma_2)) * (1. - smoothL1_sign)
         out_loss_box = bbox_outside_weights * in_loss_box
         loss_box = out_loss_box
         for i in sorted(dim, reverse=True):
@@ -185,18 +161,15 @@ class Network(nn.Module):
         rpn_cls_score = self._predictions['rpn_cls_score_reshape'].view(-1, 2)
         rpn_label = self._anchor_targets['rpn_labels'].view(-1)
         rpn_select = (rpn_label.data != -1).nonzero().view(-1)
-        rpn_cls_score = rpn_cls_score.index_select(
-            0, rpn_select).contiguous().view(-1, 2)
+        rpn_cls_score = rpn_cls_score.index_select(0, rpn_select).contiguous().view(-1, 2)
         rpn_label = rpn_label.index_select(0, rpn_select).contiguous().view(-1)
         rpn_cross_entropy = F.cross_entropy(rpn_cls_score, rpn_label)
 
         # RPN, bbox loss
         rpn_bbox_pred = self._predictions['rpn_bbox_pred']
         rpn_bbox_targets = self._anchor_targets['rpn_bbox_targets']
-        rpn_bbox_inside_weights = self._anchor_targets[
-            'rpn_bbox_inside_weights']
-        rpn_bbox_outside_weights = self._anchor_targets[
-            'rpn_bbox_outside_weights']
+        rpn_bbox_inside_weights = self._anchor_targets['rpn_bbox_inside_weights']
+        rpn_bbox_outside_weights = self._anchor_targets['rpn_bbox_outside_weights']
         rpn_loss_box = self._smooth_l1_loss(
             rpn_bbox_pred,
             rpn_bbox_targets,
@@ -208,16 +181,14 @@ class Network(nn.Module):
         # RCNN, class loss
         cls_score = self._predictions["cls_score"]
         label = self._proposal_targets["labels"].view(-1)
-        cross_entropy = F.cross_entropy(
-            cls_score.view(-1, self._num_classes), label)
+        cross_entropy = F.cross_entropy(cls_score.view(-1, self._num_classes), label)
 
         # RCNN, bbox loss
         bbox_pred = self._predictions['bbox_pred']
         bbox_targets = self._proposal_targets['bbox_targets']
         bbox_inside_weights = self._proposal_targets['bbox_inside_weights']
         bbox_outside_weights = self._proposal_targets['bbox_outside_weights']
-        loss_box = self._smooth_l1_loss(
-            bbox_pred, bbox_targets, bbox_inside_weights, bbox_outside_weights)
+        loss_box = self._smooth_l1_loss(bbox_pred, bbox_targets, bbox_inside_weights, bbox_outside_weights)
 
         self._losses['cross_entropy'] = cross_entropy
         self._losses['loss_box'] = loss_box
@@ -236,31 +207,23 @@ class Network(nn.Module):
         rpn = F.relu(self.rpn_net(net_conv))
         self._act_summaries['rpn'] = rpn
 
-        rpn_cls_score = self.rpn_cls_score_net(
-            rpn)  # batch * (num_anchors * 2) * h * w
+        rpn_cls_score = self.rpn_cls_score_net(rpn)  # batch * (num_anchors * 2) * h * w
 
         # change it so that the score has 2 as its channel size
-        rpn_cls_score_reshape = rpn_cls_score.view(
-            1, 2, -1,
-            rpn_cls_score.size()[-1])  # batch * 2 * (num_anchors*h) * w
+        rpn_cls_score_reshape = rpn_cls_score.view(1, 2, -1, rpn_cls_score.size()[-1])  # batch * 2 * (num_anchors*h) * w
         rpn_cls_prob_reshape = F.softmax(rpn_cls_score_reshape, dim=1)
 
         # Move channel to the last dimenstion, to fit the input of python functions
-        rpn_cls_prob = rpn_cls_prob_reshape.view_as(rpn_cls_score).permute(
-            0, 2, 3, 1)  # batch * h * w * (num_anchors * 2)
-        rpn_cls_score = rpn_cls_score.permute(
-            0, 2, 3, 1)  # batch * h * w * (num_anchors * 2)
-        rpn_cls_score_reshape = rpn_cls_score_reshape.permute(
-            0, 2, 3, 1).contiguous()  # batch * (num_anchors*h) * w * 2
+        rpn_cls_prob = rpn_cls_prob_reshape.view_as(rpn_cls_score).permute(0, 2, 3, 1)  # batch * h * w * (num_anchors * 2)
+        rpn_cls_score = rpn_cls_score.permute(0, 2, 3, 1)  # batch * h * w * (num_anchors * 2)
+        rpn_cls_score_reshape = rpn_cls_score_reshape.permute(0, 2, 3, 1).contiguous()  # batch * (num_anchors*h) * w * 2
         rpn_cls_pred = torch.max(rpn_cls_score_reshape.view(-1, 2), 1)[1]
 
         rpn_bbox_pred = self.rpn_bbox_pred_net(rpn)
-        rpn_bbox_pred = rpn_bbox_pred.permute(
-            0, 2, 3, 1).contiguous()  # batch * h * w * (num_anchors*4)
+        rpn_bbox_pred = rpn_bbox_pred.permute(0, 2, 3, 1).contiguous()  # batch * h * w * (num_anchors*4)
 
         if self._mode == 'TRAIN':
-            rois, roi_scores = self._proposal_layer(
-                rpn_cls_prob, rpn_bbox_pred)  # rois, roi_scores are varible
+            rois, roi_scores = self._proposal_layer(rpn_cls_prob, rpn_bbox_pred)  # rois, roi_scores are varible
             rpn_labels = self._anchor_target_layer(rpn_cls_score)
             rois, _ = self._proposal_target_layer(rois, roi_scores)
         else:
@@ -324,18 +287,14 @@ class Network(nn.Module):
         self._init_head_tail()
 
         # rpn
-        self.rpn_net = nn.Conv2d(
-            self._net_conv_channels, cfg.RPN_CHANNELS, [3, 3], padding=1)
+        self.rpn_net = nn.Conv2d(self._net_conv_channels, cfg.RPN_CHANNELS, [3, 3], padding=1)
 
-        self.rpn_cls_score_net = nn.Conv2d(cfg.RPN_CHANNELS,
-                                           self._num_anchors * 2, [1, 1])
+        self.rpn_cls_score_net = nn.Conv2d(cfg.RPN_CHANNELS,self._num_anchors * 2, [1, 1])
 
-        self.rpn_bbox_pred_net = nn.Conv2d(cfg.RPN_CHANNELS,
-                                           self._num_anchors * 4, [1, 1])
+        self.rpn_bbox_pred_net = nn.Conv2d(cfg.RPN_CHANNELS,self._num_anchors * 4, [1, 1])
 
         self.cls_score_net = nn.Linear(self._fc7_channels, self._num_classes)
-        self.bbox_pred_net = nn.Linear(self._fc7_channels,
-                                       self._num_classes * 4)
+        self.bbox_pred_net = nn.Linear(self._fc7_channels, self._num_classes * 4)
 
         self.init_weights()
 
@@ -409,10 +368,8 @@ class Network(nn.Module):
         rois, cls_prob, bbox_pred = self._predict()
 
         if mode == 'TEST':
-            stds = bbox_pred.data.new(cfg.TRAIN.BBOX_NORMALIZE_STDS).repeat(
-                self._num_classes).unsqueeze(0).expand_as(bbox_pred)
-            means = bbox_pred.data.new(cfg.TRAIN.BBOX_NORMALIZE_MEANS).repeat(
-                self._num_classes).unsqueeze(0).expand_as(bbox_pred)
+            stds = bbox_pred.data.new(cfg.TRAIN.BBOX_NORMALIZE_STDS).repeat(self._num_classes).unsqueeze(0).expand_as(bbox_pred)
+            means = bbox_pred.data.new(cfg.TRAIN.BBOX_NORMALIZE_MEANS).repeat(self._num_classes).unsqueeze(0).expand_as(bbox_pred)
             self._predictions["bbox_pred"] = bbox_pred.mul(stds).add(means)
         else:
             self._add_losses()  # compute losses
@@ -424,8 +381,7 @@ class Network(nn.Module):
       """
             # x is a parameter
             if truncated:
-                m.weight.data.normal_().fmod_(2).mul_(stddev).add_(
-                    mean)  # not a perfect approximation
+                m.weight.data.normal_().fmod_(2).mul_(stddev).add_(mean)  # not a perfect approximation
             else:
                 m.weight.data.normal_(mean, stddev)
             m.bias.data.zero_()
@@ -439,8 +395,7 @@ class Network(nn.Module):
     # Extract the head feature maps, for example for vgg16 it is conv5_3
     # only useful during testing mode
     def extract_head(self, image):
-        feat = self._layers["head"](torch.from_numpy(
-            image.transpose([0, 3, 1, 2])).to(self._device))
+        feat = self._layers["head"](torch.from_numpy(image.transpose([0, 3, 1, 2])).to(self._device))
         return feat
 
     # only useful during testing mode
@@ -474,10 +429,10 @@ class Network(nn.Module):
     def train_step(self, blobs, train_op):
         self.forward(blobs['data'], blobs['im_info'], blobs['gt_boxes'])
         rpn_loss_cls, rpn_loss_box, loss_cls, loss_box, loss = self._losses["rpn_cross_entropy"].item(), \
-                                                                            self._losses['rpn_loss_box'].item(), \
-                                                                            self._losses['cross_entropy'].item(), \
-                                                                            self._losses['loss_box'].item(), \
-                                                                            self._losses['total_loss'].item()
+        self._losses['rpn_loss_box'].item(), \
+        self._losses['cross_entropy'].item(), \
+        self._losses['loss_box'].item(), \
+        self._losses['total_loss'].item()
         #utils.timer.timer.tic('backward')
         train_op.zero_grad()
         self._losses['total_loss'].backward()
