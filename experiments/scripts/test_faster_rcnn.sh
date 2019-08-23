fcc72ead4650ee54a7d0bac87b662f27fc4bbe97
yhsmiley <46747906+yhsmiley@users.noreply.github.com>
2019-08-23 17:00:01 +0800

    changed number of iterations

--- experiments/scripts/test_faster_rcnn.sh
+++ experiments/scripts/test_faster_rcnn.sh
@@ -18,7 +18,8 @@ case ${DATASET} in
   pascal_voc)
     TRAIN_IMDB="voc_2007_trainval"
     TEST_IMDB="voc_2007_test"
-    ITERS=70000
+    # ITERS=70000
+    ITERS=1000
     ANCHORS="[8,16,32]"
     RATIOS="[0.5,1,2]"
     ;;
